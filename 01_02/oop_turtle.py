40675c6396b80e03dbfde154b0075e9917a67dd3
unknown <prandrews@hotmail.co.uk>
2021-08-13 14:19:26 +0100

    Recorded Successfully

---
+++ 01_02/oop_turtle.py
@@ -0,0 +1,13 @@
+# Import Turtle Graphics module
+import turtle
+
+# Create a turtle to do your bidding
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("red")
+
+# Your turtle awaits your command
+my_turtle.forward(100)  # Sample command
+
+# This statement (or an equivalent) is needed at the end of all your turtle programs.
+turtle.done()
---
+++ 01_02/procedural.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+color("red")
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+done()
