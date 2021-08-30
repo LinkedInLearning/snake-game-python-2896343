6648748180a6b383a5c0577d0f26a0b29e50c889
unknown <prandrews@hotmail.co.uk>
2021-08-30 12:02:43 +0100

    Retake

--- 01_03/basic_animation_example.py
+++ 01_03/basic_animation_example.py
@@ -4,27 +4,27 @@ import turtle
 # Define program constants
 WIDTH = 500
 HEIGHT = 500
-DELAY = 10  # Milliseconds between screen updates.
+DELAY = 20  # Milliseconds between screen updates.
 
 
 def move_turtle():
     my_turtle.forward(1)
     my_turtle.right(1)
     screen.update()
-    screen.ontimer(move_turtle, DELAY)  # After `DELAY` milliseconds, call move_turtle again.
+    screen.ontimer(move_turtle, DELAY)
 
 
 # Create a window where we will do our drawing.
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)  # Set the dimensions of the Turtle Graphics window.
-screen.title("Turtle Animation")
+screen.title("Animation Example")
 screen.bgcolor("cyan")
+screen.tracer(0)  # Turns off automatic animation
 
 # Create a turtle to do your bidding
 my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("red")
-screen.tracer(0)  # Turn off automatic animation
 
 # Set animation in motion
 move_turtle()
