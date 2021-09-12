192b56b66c5de5f3dbac49d5f5e8702faa2b33c5
unknown <prandrews@hotmail.co.uk>
2021-09-12 16:22:27 +0100

    Recorded successfully

--- 01_05/stamps.py
+++ 01_05/stamps.py
@@ -8,16 +8,19 @@ HEIGHT = 500
 # Create a window where we will do our drawing.
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)  # Set the dimensions of the Turtle Graphics window.
-screen.title("Program Title")
+screen.title("Stamping")
 screen.bgcolor("cyan")
 
 # Create a turtle to do your bidding
-my_turtle = turtle.Turtle()
-my_turtle.shape("turtle")
-my_turtle.color("red")
-
-# Your turtle awaits your command
-my_turtle.forward(100)  # Sample command
+stamper = turtle.Turtle()
+stamper.shape("square")
+stamper.color("red")
+stamper.shapesize(50 / 20)
+stamper.stamp()
+stamper.penup()
+stamper.shapesize(10 / 20)
+stamper.goto(100, 100)
+stamper.stamp()
 
 # This statement (or an equivalent) is needed at the end of all your turtle programs.
 turtle.done()
